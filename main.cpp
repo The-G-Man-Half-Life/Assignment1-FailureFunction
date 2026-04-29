27c7b0ebe06b22ece4da2d59392774cae96cee9b
The-G-Man-Half-Life <mateomoospina@gmail.com>
2026-04-28 21:09:56 -0500

    feat: added menu and good visualization

--- main.cpp
+++ main.cpp
@@ -1,43 +1,123 @@
 //Includes for all the required elements
 #include "iostream"
+#include <string>
+#include <iomanip>
+#include <limits>
 #include "vector"
 
 //Execution of the code itself
 std::vector<int> findFailureSpotsKMP(const std::string& input) {
-    int n = input.size();
-    std::vector<int> failurePlaces(n, 0); // Todo inicia en 0
-    
-    int len = 0; // Tamaño del prefijo anterior
-    int i = 1;   // Posición actual (currentPosition)
+    //Variables
+    int inputSize = input.size();
+    std::vector<int> failurePlaces(inputSize, 0); // All the values in the vector started as 0
+    int lastPrefixLength = 0; // Size of the last prefix
+    int currentPosition = 1;   // Current position executing at
 
-    while (i < n) {
-        // Si las LETRAS coinciden, el tamaño crece en 1
-        if (input[i] == input[len]) {
-            len++;
-            failurePlaces[i] = len;
-            i++;
+    //cycle to go through the entirety of the input comparing
+    while (currentPosition < inputSize) {
+        // if the letters do match the current position increases by 1
+        if (input[currentPosition] == input[lastPrefixLength]) {
+            lastPrefixLength++;
+            //Inserts value into the vector
+            failurePlaces[currentPosition] = lastPrefixLength;
+            currentPosition++;
         } 
         else {
-            // Si NO coinciden, aquí está la magia del KMP en vez de usar un ciclo "for"
-            if (len != 0) {
-                // Retrocedemos al fallo anterior en lugar de probar uno por uno
-                len = failurePlaces[len - 1]; 
+            /*If these values do not match then we go to the last failing position
+            so we don't have to review everything 1 by 1*/
+            if (lastPrefixLength != 0) {
+                lastPrefixLength = failurePlaces[lastPrefixLength - 1]; 
             } 
             else {
-                // Si llegamos a 0, simplemente no hay coincidencia
-                failurePlaces[i] = 0;
-                i++;
+                /*In case we arrive to 0 it means that there is no match*/
+                failurePlaces[currentPosition] = 0;
+                currentPosition++;
             }
         }
     }
     return failurePlaces;
 }
 
-int main(){
-    std::vector<int> failureSpotsKMP = findFailureSpotsKMP("ababaa");
+//Function to print more organizedly
+void printKMP(const std::vector<int>& failureSpotsKMP, const std::string& input) {
+    int inputSize = input.size();
 
-    for (int number : failureSpotsKMP){
-        std::cout << number << std::endl;
+    //Calculate the width dinamicly
+    int maxDigits = std::to_string(inputSize).length();
+    int sWidth = std::max(4, maxDigits + 2); 
+    int pWidth = 6; 
+    int fWidth = 6; 
+
+    //print the headers (s | P[s] | f(s))
+    std::cout << std::right << std::setw(sWidth) << "s" << " | "
+              << std::setw(pWidth) << "P[s]" << " | "
+              << std::setw(fWidth) << "f(s)" << std::endl;
+
+    //Print separating lines
+    int totalWidth = sWidth + pWidth + fWidth + 6; 
+    std::cout << std::string(totalWidth, '-') << std::endl;
+
+    //Print data line by line
+    for (int i = 0; i < inputSize; i++) {
+        std::cout << std::right << std::setw(sWidth) << (i + 1) << " | "
+                  << std::setw(pWidth) << input[i] << " | "
+                  << std::setw(fWidth) << failureSpotsKMP[i] << std::endl;
     }
+}
+
+int main(){
+    //Variables
+    std::vector<int> failureSpotsKMP;
+    std::string input;
+    std::string decision;
     
+    while(true){
+        //stopping the program for a bit to execute next line
+        std::cout << "Ingrese enter para continuar";
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin.get();
+        std::cout << "" << std::endl;
+
+        //showing menu and receiving decision
+        std::cout << "Algoritmo KMP" << std::endl;
+        std::cout << "1. Ingresar Palabra Propia" << std::endl;
+        std::cout << "2. Soluciones ejercicio 3.4.3" << std::endl;
+        std::cout << "(Cualquier otra entrada) Terminar programa" << std::endl;
+        std::cout << "Ingrese su opcion: ";
+        std::cin >> decision;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "" << std::endl;
+
+    
+        if(decision == "1"){
+        //Receiving the input from the user
+        std::cout << "Ingrese una palabra: ";
+        std::cin >> input;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        //getting the vector 
+        failureSpotsKMP = findFailureSpotsKMP(input);
+        //printing result
+        std::cout << "Esta es la Palabra " << input << " Procesada por el algoritmo KMP" << std::endl;
+        printKMP(failureSpotsKMP, input);
+        std::cout << "" << std::endl;
+        }
+        else if (decision == "2")
+        {
+            // set of words to check from exercise 3.4.3
+            std::vector<std::string> words = {"abababaab", " aaaaaa", "abbaabb"};
+            for (std::string word: words){
+                //Getting the vector of integers from the function
+                failureSpotsKMP = findFailureSpotsKMP(word);
+                //printing results
+                std::cout << "Esta es la Palabra " << word << " Procesada por el algoritmo KMP" << std::endl;   
+                printKMP(failureSpotsKMP, word);  
+                std::cout << "" << std::endl;       
+            }   
+        }
+        else{
+            //finishing program
+            break;
+        }
+    }
 }
